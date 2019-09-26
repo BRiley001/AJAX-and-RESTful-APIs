087e70a2b4b962875d9404cfa3a0bf481d3a5dae
Brenden Riley <BRiley001@west-mec.org>
2019-09-26 14:06:57 -0700

    Project 1 done
    Project 1 works as far as I know it should

--- Projects/Project_Lab/www/StockCheck.php
+++ Projects/Project_Lab/www/StockCheck.php
@@ -1,8 +1,13 @@
 <?php
-$TickerSymbol = $_GET["t"];
-header("Cache-Control: no-cache");
-header("Content-Type: text/csv");
-$Quote = "http://quote.yahoo.com/d/quotes.csv?s=$TickerSymbol&f=sl1d1t1c1p2ohgv";
-$QuoteString = file_get_contents($Quote);
-echo $QuoteString;
+    $TickerSymbol = $_GET["t"];
+    $DateOne = $_GET["s"];
+    $DateTwo = $_GET["e"];
+
+    header("Cache-Control: no-cache");
+    header("Content-Type: text/csv");
+
+    $Quote = "https://www.quandl.com/api/v3/datasets/WIKI/$TickerSymbol.json?end_date=$DateTwo&start_date=$DateOne";
+    $QuoteString = file_get_contents($Quote);
+
+    echo $QuoteString;
 ?>
--- Projects/Project_Lab/www/script.js
+++ Projects/Project_Lab/www/script.js
@@ -10,20 +10,89 @@
 var httpRequest = false;
 var entry = "^IXIC";
 
-function getRequestObject(){
+function getRequestObject() {
     try {
         httpRequest = new XMLHttpRequest();
-    }
-    catch (requestError) {
+    } catch (requestError) {
         return false;
     }
-    alert(httpRequest);
     return httpRequest;
 }
 
-if (window.addEventListener) {
-    window.addEventListener("load", getRequestObject, false);
+function getMonth(month) {
+    var curMonth = month > 0 ? month + 1 : 12;
+    return curMonth < 10 ? "0" + curMonth : curMonth;
+}
+
+var form = document.getElementsByTagName("form")[0];
+if (form.addEventListener) {
+    form.addEventListener("submit", stopSubmission, false);
+    window.addEventListener("load", formatTable, false);
+    window.addEventListener("load", getQuote, false);
 } 
-else if (window.attachEvent) {
-    window.attachEvent("onload", getRequestObject);
+else if (form.attachEvent) {
+    form.attachEvent("onsubmit", stopSubmission);
+    window.attachEvent("onload", formatTable);
+    window.attachEvent("onload", getQuote);
+}
+
+function stopSubmission(evt) {
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    } else {
+        evt.returnValue = false;
+    }
+    getQuote();
+}
+
+function getQuote() {
+    if (document.getElementsByTagName("input")[0].value) {
+        entry = document.getElementsByTagName("input")[0].value;
+    }
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
+    let current = new Date();
+    let threeDaysAgo = new Date();
+    threeDaysAgo.setDate(current.getDate() - 3);
+    let dateOne = `2017-${getMonth(threeDaysAgo.getMonth())}-${threeDaysAgo.getDate()}`;
+    let dateTwo = `2017-${getMonth(current.getMonth())}-${current.getDate()}`
+
+    httpRequest.abort();
+    httpRequest.open("get", `StockCheck.php?t=${entry}&s=${dateOne}&e=${dateTwo}`, true);
+    httpRequest.send(null);
+    httpRequest.onreadystatechange = displayData;
+}
+
+function displayData() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        var stockResults = httpRequest.responseText;
+        console.log(stockResults);
+        var stockItems;
+        try {
+            stockItems = JSON.parse(stockResults);
+        } catch (error) {
+            document.getElementById("ticker").innerHTML = "Error: Invalid Ticker.";
+            document.getElementById("openingPrice").innerHTML = "-";
+            document.getElementById("lastTrade").innerHTML = "-";
+            document.getElementById("lastTradeDT").innerHTML = "-";
+            document.getElementById("change").innerHTML = "-";
+            document.getElementById("range").innerHTML = "-";
+            document.getElementById("volume").innerHTML = "-";
+            return;
+        }
+        document.getElementById("ticker").innerHTML = stockItems.dataset.dataset_code;
+        document.getElementById("openingPrice").innerHTML = stockItems.dataset.data[0][1];
+        document.getElementById("lastTrade").innerHTML = stockItems.dataset.data[1][4];
+        document.getElementById("lastTradeDT").innerHTML = stockItems.dataset.data[1][0].replace("2019", new Date().getFullYear());
+        document.getElementById("change").innerHTML = ((parseFloat(stockItems.dataset.data[1][4]) - parseFloat(stockItems.dataset.data[0][1]))).toFixed(2);
+        document.getElementById("range").innerHTML = "Low " + stockItems.dataset.data[0][3] + "<br>High " + stockItems.dataset.data[0][2];
+        document.getElementById("volume").innerHTML = stockItems.dataset.data[0][5];
+    }
+}
+function formatTable() {
+    var rows = document.getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].style.background = "#9FE098";
+    }
 }
